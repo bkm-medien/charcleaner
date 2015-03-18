d0202db4936572b4e84b6992b92cc39f22a45076
Alfred Bez <a.bez@bkm-medien.de>
2015-03-18 09:49:41 +0100

    Dokumentation

--- src/Cleaner.php
+++ src/Cleaner.php
@@ -6,8 +6,22 @@ use ForceUTF8\Encoding;
 
 class Cleaner
 {
+    /**
+     * 'suchen und ersetzen' Regeln in folgender Form
+     *     array(
+     *         'suchen'  => 'ersetzen',
+     *         'suchen1' => 'ersetzen1',
+     *     ).
+     *
+     * @var array
+     */
     public $cleanTerms = [];
 
+    /**
+     * Instanz von \League\CLImate\CLImate.
+     *
+     * @var \League\CLImate\CLImate
+     */
     protected $cli;
 
     public function __construct()
@@ -15,6 +29,29 @@ class Cleaner
         $this->cli = new CLImate();
     }
 
+    /**
+     * Alle Dateien aus dem Array $files auslesen und Regeln aus $cleanTerms
+     * anwenden. Dabei werden Statistiken über die verarbeitet und bereinigten
+     * Dateien gesammelt und übergeben.
+     * Außerdem werden mögliche Sonderzeichen erkannt und zurückgegeben.
+     * Wenn man die Methode über das CLI aufruft, zeigt eine Progressbar den
+     * Fortschritt an.
+     *
+     * @param array $files zu bereinigende Dateien (Pfad zur Datei)
+     *
+     * @return array Statistiken in folgender Form
+     *               array(
+     *               'checked' => 100, // Anzahl aller geprüften Dateien
+     *               'cleaned' => 10, // Anzahl aller bereinigten Dateien
+     *               'warningTerms' => array(
+     *               array(
+     *               'matches' => '&#123;', // gefundenes Sonderzeichen
+     *               'file' => 'files/index.html', // Datei, in der das
+     *               //Sonderzeichen gefunden wurde
+     *               )
+     *               )
+     *               )
+     */
     public function clean($files)
     {
         $stats['cleaned'] = 0;
